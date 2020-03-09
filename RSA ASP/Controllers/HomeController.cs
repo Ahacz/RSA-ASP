1caf50c321ce435c57458c0c7df20214e09058b1
Ahacz <37111467+Ahacz@users.noreply.github.com>
2020-03-09 20:33:40 +0100

    RSA cryptography project
    An ASP.NET based project that can create a key pair, sign files and verify signatures.

--- RSA ASP/Controllers/HomeController.cs
+++ RSA ASP/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,9 +14,67 @@ namespace RSA_ASP.Controllers
     {
         public ActionResult Index()
         {
+            return RedirectToAction("UploadFile");
+        }
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
             return View();
         }
+        [HttpPost]
+        public ActionResult GenerateRSA()
+        {
+            AsymmetricCipherKeyPair newkeys = RSASigner.GetKeyPairWithDotNet();
+            this.Session["keys"] = newkeys;
+            ViewBag.pubkey = RSASigner.FormatToPEM(newkeys.Public);
+            ViewBag.privkey = RSASigner.FormatToPEM(newkeys.Private);
+            return View("UploadFile");
+        }
+        public ActionResult SignFile(HttpPostedFileBase file,String[] RSAPriv,string [] RSAPub)
+        {
+                if (file != null && file.ContentLength > 0)
+                {
+                    MemoryStream target = new MemoryStream();
+                    file.InputStream.CopyTo(target);
+                    byte[] data = target.ToArray();
+                    ViewBag.rsasig=RSASigner.Sign(data,RSAPriv);
+                    ViewBag.Message = "Podpisano!";
+                    return View("UploadFile");
+            }
+                ViewBag.Message="Błąd wysyłania pliku.";
+                return View("UploadFile");
 
+        }
+        public ActionResult VerifySignature(HttpPostedFileBase file, String[] RSAPriv, string[] RSAPub, string [] Sign)
+        {
+            if (file.ContentLength > 0)
+            {
+                MemoryStream target = new MemoryStream();
+                file.InputStream.CopyTo(target);
+                byte[] data = target.ToArray();
+                RsaKeyParameters pubk;
+                using (var stringReader = new StringReader(string.Join("\n", RSAPub)))
+                {
+                    var pemReader = new PemReader(stringReader);
+                    var pemObject = pemReader.ReadObject(); // null!
+                    pubk = (RsaKeyParameters)pemObject;
+                }
+                try
+                {
+                    if (RSASigner.VerifySignedHash(data, Convert.FromBase64String(string.Join("\n", Sign)), pubk))
+                        ViewBag.Message = "Zgadza się!";
+                    else ViewBag.Message = "Błąd weryfikacji";
+                    return View("UploadFile");
+                }
+                catch
+                {
+                    ViewBag.Message = "Błędny podpis/klucz";
+                    return View("UploadFile");
+                }
+            }
+            ViewBag.Message = "Błąd wysyłania pliku.";
+            return View("UploadFile");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ RSA ASP/RSASigner.cs
@@ -0,0 +1,80 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace RSA_ASP
+{
+    public class RSASigner
+    {
+        public static AsymmetricCipherKeyPair GetKeyPairWithDotNet()
+        {
+            using (RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider())
+            {
+                RsaKeyPairGenerator r = new RsaKeyPairGenerator();
+                r.Init(new KeyGenerationParameters(new SecureRandom(),
+                      1024));
+                AsymmetricCipherKeyPair keys = r.GenerateKeyPair();
+                return keys;
+            }
+        }
+        public static string FormatToPEM(AsymmetricKeyParameter item)
+        {
+            TextWriter textWriter = new StringWriter();
+            PemWriter pemWriter = new PemWriter(textWriter);
+            pemWriter.WriteObject(item);
+            pemWriter.Writer.Flush();
+            return textWriter.ToString();
+        }
+        public static AsymmetricCipherKeyPair ReadFromPem(string pemEncodedKey)
+        {
+            AsymmetricCipherKeyPair result = null;
+            using (var stringReader = new StringReader(pemEncodedKey))
+            {
+                var pemReader = new PemReader(stringReader);
+                var pemObject = pemReader.ReadObject(); // null!
+                result = (AsymmetricCipherKeyPair)pemObject;
+            }
+
+            return result;
+        }
+
+        public static String Sign(byte[] data, string [] RSAPriv)
+        {
+
+            string input = string.Join("\n", RSAPriv);
+            AsymmetricCipherKeyPair givenKey = ReadFromPem(input);
+            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+            var rsaParameters = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)givenKey.Private);
+            RSAalg.ImportParameters(rsaParameters);
+            var sha = new SHA1CryptoServiceProvider();
+            byte[] hash = sha.ComputeHash(data);
+            var sig = RSAalg.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
+            return Convert.ToBase64String(sig);
+        }
+
+        public static bool VerifySignedHash(byte[] DataToVerify, byte[] SignedData, RsaKeyParameters key)
+        {
+            try
+            {
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                var rsaParameters = DotNetUtilities.ToRSAParameters(key);
+                RSAalg.ImportParameters(rsaParameters);
+                return RSAalg.VerifyData(DataToVerify, new SHA1CryptoServiceProvider(), SignedData);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
